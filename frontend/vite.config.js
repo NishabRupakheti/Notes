c37463478183cb3151ece3485ebe64e32d3164f2
Nishab <nishabrupakheti2@gmail.com>
2025-01-03 21:12:30 +0200

    Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',   // Allow external access
+    port: 5173,         // The port Vite is running on
+    strictPort: true,   // Make sure the port is not in use by another process
+  },
   plugins: [react()],
 })
